0cdf7ea64b151e97af4904ba3ef1c09b2d0f2394
Andrey Feofilaktov <andarpolis@gmail.com>
2018-04-22 13:38:55 +0300

    supported simple showing tasks

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,9 @@
 
 module Main where
 
+import Debug.Trace
+import Control.Monad.IO.Class
+
 import GHC.Int
 import System.Environment
 import Data.Text (Text)
@@ -27,32 +30,33 @@ type TaskId = GHC.Int.Int64
 
 data Database = Database String
 
-data Model = Model
-
 data Action
   = NoOp
   | Start
   | AddTask Task
   | RemoveTask Task
-  | Show Text
+  | Show
   deriving (Show, Read)
 
 data TasksStorage = TasksStorage
   { addTask :: Task -> IO TaskId
   , removeTask :: TaskId -> IO ()
-  , showTask :: TaskId -> IO Task
+  , getTask :: TaskId -> IO Task
+  , getAllTasks :: IO [Task]
   }
+
+data Model = Model TasksStorage
 
 getTasksStorage = TasksStorage
   { addTask = addTask_
   , removeTask = removeTask_
-  , showTask = showTask_
+  , getTask = getTask_
+  , getAllTasks = getAllTasks_
   }
   where
     addTask_ task = do
       conn <- open database
-      execute conn "insert into Tasks (Text) values(?)"
-        (Only task)
+      execute conn "insert into Tasks (Text) values(?)" (Only task)
       taskId <- lastInsertRowId conn
       close conn
       return taskId
@@ -62,11 +66,17 @@ getTasksStorage = TasksStorage
       execute conn "delete from Tasks where TaskId = (?)" (Only taskId)
       close conn
 
-    showTask_ taskId = do
+    getTask_ taskId = do
       conn <- open database
       [Only result] <- query conn "select Text from Tasks where TaskId = (?)" (Only taskId) :: IO [Only Task]
       close conn
       return result
+
+    getAllTasks_ = do
+      conn <- open database
+      result <- query_ conn "select Text from Tasks" :: IO [Only Task]
+      close conn
+      return $ map fromOnly result
 
 setupDatabase :: Database -> IO ()
 setupDatabase (Database database) = do
@@ -76,7 +86,7 @@ setupDatabase (Database database) = do
 
 taskManager :: BotApp Model Action
 taskManager = BotApp
-  { botInitialModel = Model
+  { botInitialModel = Model getTasksStorage
   , botAction = flip updateToAction
   , botHandler = handleAction
   , botJobs = []
@@ -88,29 +98,26 @@ taskManager = BotApp
       <|> Start        <$  command "start"
       <|> AddTask      <$> command "add"
       <|> RemoveTask   <$> command "remove"
-      <|> Show         <$> command "show"
+      <|> Show         <$ command "show"
       <|> callbackQueryDataRead
 
     handleAction :: Action -> Model -> Eff Action Model
-    handleAction action model = case action of
-      NoOp -> pure model
-      Start -> model <# do
+    handleAction action (Model tasksStorage) = case action of
+      NoOp -> pure (Model tasksStorage)
+      Start -> Model tasksStorage <# do
         reply "Start"
         return NoOp
-      AddTask task -> model <# do
-        reply "Add"
+      AddTask task -> Model tasksStorage <# do
+        taskId <- liftIO $ addTask tasksStorage task
+        reply . toReplyMessage . Text.pack $ "Added " ++ (show taskId)
         return NoOp
-      RemoveTask task -> model <# do
-        reply "Remove"
+      RemoveTask task -> Model tasksStorage <# do
+        reply "Task removed"
         return NoOp
-      Show task -> model <#do
-        reply "Show"
+      Show -> Model tasksStorage <# do
+        tasks <- liftIO $ getAllTasks tasksStorage
+        reply . toReplyMessage . Text.concat $ ["Show \n", (Text.unlines tasks)]
         return NoOp
-
-run :: TasksStorage -> Task -> IO Task
-run (TasksStorage {showTask, removeTask, addTask}) task = do
-  taskId <- addTask task
-  showTask taskId
 
 runBot :: Token -> IO ()
 runBot token = do
@@ -120,8 +127,5 @@ runBot token = do
 main :: IO ()
 main = do
   setupDatabase (Database database)
-  inputText <- TextIO.getLine
-  textInBase <- run (getTasksStorage) inputText
-  TextIO.putStrLn textInBase
   token <- getEnv $ "TELEGRAM_TOKEN"
   runBot . Token . Text.pack $ token
